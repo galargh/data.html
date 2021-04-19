27a3e1c9dff1d987dd4b67814451d90ac90a1605
Piotr Galar <pgalar@sumologic.com>
2021-04-19 21:24:50 +0200

    Release org.clojars.gfjalar/data.html@0.1.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,10 @@
 All notable changes to this project will be documented in this file. This change log follows the conventions of [keepachangelog.com](http://keepachangelog.com/).
 
 ## [Unreleased]
-### Changed
+
+## [0.1.0]
+### Added
 - Add initial implementation for `Tokenizable` protocol.
 
 [Unreleased]: https://github.com/gfjalar/data.html/
+[0.1.0]: https://github.com/gfjalar/data.html//releases/tag/v0.1.0
--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject data.html "0.1.0-SNAPSHOT"
+(defproject org.clojars.gfjalar/data.html "0.1.0"
   :description "Library for HTML data tokenization."
   :url "https://github.com/gfjalar/data.html/"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT License"
+            :url "https://opensource.org/licenses/MIT/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.namespace "1.1.0"]
                  [com.clojure-goes-fast/clj-java-decompiler "0.1.0"]]
