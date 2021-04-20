f95a2cacb3305693e704ae79774d77c767cf8003
Piotr Galar <pgalar@sumologic.com>
2021-04-20 19:48:45 +0200

    Remove unused dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,7 @@
   :url "https://github.com/gfjalar/data.html/"
   :license {:name "MIT License"
             :url "https://opensource.org/licenses/MIT/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [org.clojure/tools.namespace "1.1.0"]
-                 [com.clojure-goes-fast/clj-java-decompiler "0.1.0"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]]
   :plugins [[lein-zprint "1.1.1"]]
   :zprint {:old? false}
   :repl-options {:init-ns data.html})
